e826bdc035dcefb50670832a4318e49cc24ef9e0
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-11-20 09:41:20 +0200

    feat: :zap: update login page responsive

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -32,7 +32,7 @@ const Login = () => {
   return (
     <section
       className="bg-custom-image bg-cover lg:h-full
-     bg-center bg-no-repeat py-24 mt-24 max-md:py-8 max-xs:py-12 max-sm:mt-20  "
+     bg-center bg-no-repeat py-24 mt-24 max-sm:mt-20 max-sm:h-screen  "
     >
       <div className="flex justify-center items-center w-full h-full">
         <div className="bg-white rounded-xl shadow-md lg:py-20 lg:px-10 md:w-2/5 max-md:w-3/5 max-xs:w-4/5 max-sm:w-4/5 max-xs:py-10 2xl:w-1/3 md:py-10 md:px-6 xs:py-14 max-md:px-5 ">
