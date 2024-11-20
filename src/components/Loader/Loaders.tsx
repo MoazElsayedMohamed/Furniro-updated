1087773d50b076c3b5caf50169ca0b708fa5041a
MOAZ ELSAYED <moazelsayed75@yahoo.com>
2024-11-20 09:43:17 +0200

    update loading shimmer

--- src/components/Loader/Loaders.tsx
+++ src/components/Loader/Loaders.tsx
@@ -8,7 +8,7 @@ interface Props {
 const Loaders: React.FC<Props> = ({ numberOfLoaders }) => {
   const loaders = Array(numberOfLoaders).fill(0);
   return (
-    <div className={`grid grid-cols-${numberOfLoaders}`}>
+    <div className="grid grid-cols-3">
       {loaders.map((_, index) => (
         <Loader key={index} />
       ))}
